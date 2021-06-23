2fddb0f366d644d5b28ef328b9dce6ed910a8f23
eddieciou <a589658gur@gmail.com>
2021-06-23 21:47:30 +0800

    setting.py add postgres

--- mydjan/settings.py
+++ mydjan/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'mydjan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        # 'HOST': 'pg',     # Host name in Docker
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
