6c386d9ae7c92e458bcc84f4825b43029087d40f
eddieciou <a589658gur@gmail.com>
2021-07-05 20:38:31 +0800

    change timezone setting

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse(f"Hello, world. You're at the polls index. {datetime.now()}")
