b1c982c5524671642906494942835303a84431ea
eddieciou <a589658gur@gmail.com>
2021-07-28 11:35:38 +0800

    polls add more testcase for was_published_recently

--- polls/tests.py
+++ polls/tests.py
@@ -7,6 +7,15 @@ from .models import Question
 
 
 class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_old_question(self):
+        old_time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        old_question = Question(pub_date=old_time)
+        self.assertIs(old_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_question(self):
+        recent_time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_question = Question(pub_date=recent_time)
+        self.assertIs(recent_question.was_published_recently(), True)
 
     def test_was_published_recently_with_feature_question(self):
         """
